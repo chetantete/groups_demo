c5570793c30c32571952524f5fadae20dfa5ed08
chetantete <chetan.tete@gmail.com>
2020-09-21 06:09:03 +0530

    scheduler time updated

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,9 +2,8 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-# scheduler.every '5m' do
-scheduler.every '2h' do
-	puts 'started!!!'
-	FetchFbDataJob.perform_now
+scheduler.every '2m' do
+  puts 'started!!!'
+  FetchFbDataJob.perform_now
   puts 'completed..!'
 end
